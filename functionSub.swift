d2f6416d95c8b358f94e1f718958e6d2efbd6f9b
ilpol <ilya-polozov@mail.ru>
2022-09-25 20:51:01 +0300

    fix func syntax in print

--- functionSub.swift
+++ functionSub.swift
@@ -1,4 +1,4 @@
 func sub2(first: Int, second: Int) -> Int {
     return first - second
 }
-print("funcc sub2: sum of 5 and 5 = ", sub2(first: 5,second: 5))
+print("func sub2: sum of 5 and 5 = ", sub2(first: 5,second: 5))
